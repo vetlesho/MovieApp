486bc3245fd96e616e824c5be2104d97408e3ea4
vetlesho <vetlesho@stud.ntnu.no>
2025-08-13 14:25:23 +0200

    Remove unused files, and updated README

--- MovieApp/obj/Debug/net9.0/MovieApp.AssemblyInfo.cs
+++ MovieApp/obj/Debug/net9.0/MovieApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1771e1d2a4c64d6de17c38faa6a840082b29ed5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d3c8e81225427792c59738240f6ae1e2796136")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
